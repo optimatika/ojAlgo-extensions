fcf502b0164c4197f750c156adfb4ff6da040f77
apete <apete@optimatika.se>
2017-02-01 21:26:34 +0100

    2017-02-01

---
+++ ojAlgo-unsafe/src/test/java/org/ojalgo/array/BasicArrayTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 1997-2016 Optimatika (www.optimatika.se)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.ojalgo.array;
+
+import java.util.Arrays;
+
+import org.ojalgo.random.Uniform;
+
+/**
+ * AbstractArrayTest
+ *
+ * @author apete
+ */
+public abstract class BasicArrayTest extends ArrayTests {
+
+    static final int COUNT = 100;
+    static final long[] INDICES = new long[10];
+
+    static {
+        for (int i = 0; i < INDICES.length; i++) {
+            INDICES[i] = Uniform.randomInteger(COUNT);
+        }
+        Arrays.sort(INDICES);
+    }
+
+    public BasicArrayTest() {
+        super();
+    }
+
+    public BasicArrayTest(final String aName) {
+        super(aName);
+    }
+
+    public void testPrimitive64() {
+        this.doTest(Primitive64Array.make(COUNT));
+    }
+
+    public void testPrimitive32() {
+        this.doTest(Primitive32Array.make(COUNT));
+    }
+
+    public void testNative64() {
+        this.doTest(OffHeapArray.makeNative64(COUNT));
+    }
+
+    public void testNative32() {
+        this.doTest(OffHeapArray.makeNative32(COUNT));
+    }
+
+    public void testSegmentedPrimitive() {
+        //this.doTest(SegmentedArray.make(PrimitiveArray.FACTORY, COUNT));
+        this.doTest(SegmentedArray.makePrimitiveDense(COUNT));
+    }
+
+    public void testSegmentedSparse() {
+        //this.doTest(SparseArray.makePrimitiveSegmented(COUNT));
+        this.doTest(SegmentedArray.makePrimitiveSparse(COUNT));
+    }
+
+    public void testSparse() {
+        this.doTest(SparseArray.makePrimitive(COUNT));
+    }
+
+    public void testHugeSparse() {
+        this.doTest(SparseArray.makePrimitive(Long.MAX_VALUE, COUNT));
+    }
+
+    abstract void doTest(final BasicArray<Double> array);
+
+}
---
+++ ojAlgo-unsafe/src/test/java/org/ojalgo/array/NumberListTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 1997-2016 Optimatika (www.optimatika.se)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.ojalgo.array;
+
+import org.ojalgo.TestUtils;
+
+/**
+ * NumberListTest
+ *
+ * @author apete
+ */
+public class NumberListTest extends ArrayTests {
+
+    public NumberListTest() {
+        super();
+    }
+
+    public NumberListTest(final String aName) {
+        super(aName);
+    }
+
+    public void testGrowCapacity() {
+
+        final NumberList<Double> tmNumberList = NumberList.make(OffHeapArray.NATIVE64);
+
+        TestUtils.assertEquals(0L, tmNumberList.count());
+        TestUtils.assertEquals(16L, tmNumberList.capacity());
+
+        for (long i = 0L; i <= 16; i++) {
+            tmNumberList.add(i);
+        }
+
+        TestUtils.assertEquals(17L, tmNumberList.count());
+        TestUtils.assertEquals(32L, tmNumberList.capacity());
+
+        for (long i = 17L; i <= 16_384L; i++) {
+            tmNumberList.add(i);
+        }
+
+        TestUtils.assertEquals(16_385L, tmNumberList.count());
+        TestUtils.assertEquals(16_384L * 2L, tmNumberList.capacity());
+    }
+
+}
