a3d8c98c228692da7a61a7dee9b89a35dbaae96b
apete <apete@optimatika.se>
2017-04-26 18:08:08 +0200

    2017-04-26

--- ojAlgo-spark/src/main/java/org/ojalgo/spark/rdd/MatrixRDD.java
+++ ojAlgo-spark/src/main/java/org/ojalgo/spark/rdd/MatrixRDD.java
@@ -26,30 +26,76 @@ import org.apache.spark.Partition;
 import org.apache.spark.SparkContext;
 import org.apache.spark.TaskContext;
 import org.apache.spark.rdd.RDD;
-import org.ojalgo.matrix.store.MatrixStore;
+import org.ojalgo.access.Access2D;
+import org.ojalgo.access.Mutate2D;
+import org.ojalgo.access.Mutate2D.Receiver;
+import org.ojalgo.access.Stream2D;
+import org.ojalgo.function.BinaryFunction;
+import org.ojalgo.function.UnaryFunction;
 
 import scala.collection.Iterator;
 import scala.collection.Seq;
 import scala.reflect.ClassTag;
 
-public class MatrixStoreRDD<N extends Number> extends RDD<MatrixStore<N>> {
+public final class MatrixRDD<N extends Number> extends RDD<N> implements Stream2D<N, Access2D<N>, Mutate2D.Receiver<N>, MatrixRDD<N>> {
 
-    public MatrixStoreRDD(final RDD<?> oneParent, final ClassTag<MatrixStore<N>> evidence$2) {
+    public static void main() {
+
+        final MatrixRDD rdd = null;
+
+    }
+
+    public MatrixRDD(final RDD<?> oneParent, final ClassTag<N> evidence$2) {
         super(oneParent, evidence$2);
     }
 
-    public MatrixStoreRDD(final SparkContext _sc, final Seq<Dependency<?>> deps, final ClassTag<MatrixStore<N>> evidence$1) {
+    public MatrixRDD(final SparkContext _sc, final Seq<Dependency<?>> deps, final ClassTag<N> evidence$1) {
         super(_sc, deps, evidence$1);
     }
 
+    public void supplyTo(final Receiver<N> receiver) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public long countColumns() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public long countRows() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public MatrixRDD<N> operateOnAll(final UnaryFunction<N> operator) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public MatrixRDD<N> operateOnMatching(final Access2D<N> left, final BinaryFunction<N> operator) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public MatrixRDD<N> operateOnMatching(final BinaryFunction<N> operator, final Access2D<N> right) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
     @Override
-    public Iterator<MatrixStore<N>> compute(final Partition arg0, final TaskContext arg1) {
+    public Iterator<N> compute(final Partition arg0, final TaskContext arg1) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Partition[] getPartitions() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public MatrixRDD<N> transpose() {
         // TODO Auto-generated method stub
         return null;
     }
