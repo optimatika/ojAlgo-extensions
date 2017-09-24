c8b8bd367dc2acee510824b7f12ce17ed3d7f4b4
apete <apete@optimatika.se>
2017-09-24 12:20:11 +0200

    2017-09-24

---
+++ ojAlgo-commons-math3/src/main/java/org/ojalgo/commons/math3/optim/linear/CommonsMathSimplexSolver.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 1997-2017 Optimatika (www.optimatika.se)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.ojalgo.commons.math3.optim.linear;
+
+import org.apache.commons.math3.optim.linear.SimplexSolver;
+import org.ojalgo.optimisation.ExpressionsBasedModel;
+import org.ojalgo.optimisation.Optimisation;
+
+public final class CommonsMathSimplexSolver implements Optimisation.Solver {
+
+    public static final ExpressionsBasedModel.Integration<CommonsMathSimplexSolver> INTEGRATION = new ExpressionsBasedModel.Integration<CommonsMathSimplexSolver>() {
+
+        public CommonsMathSimplexSolver build(final ExpressionsBasedModel model) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public boolean isCapable(final ExpressionsBasedModel model) {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+    };
+
+    private final SimplexSolver myDelegate = null;
+
+    public Result solve(final Result kickStarter) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
