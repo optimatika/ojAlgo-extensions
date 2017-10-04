a01241b9fae896dd2ad7298ee9eaf60e6973ce2c
apete <anders.peterson@scila.se>
2017-10-04 16:27:27 +0200

    2017-10-04

--- ojAlgo-commons-math3/src/main/java/org/ojalgo/commons/math3/optim/linear/CommonsMathSimplexSolver.java
+++ ojAlgo-commons-math3/src/main/java/org/ojalgo/commons/math3/optim/linear/CommonsMathSimplexSolver.java
@@ -21,31 +21,80 @@
  */
 package org.ojalgo.commons.math3.optim.linear;
 
+import java.util.List;
+
+import org.apache.commons.math3.optim.InitialGuess;
+import org.apache.commons.math3.optim.OptimizationData;
+import org.apache.commons.math3.optim.PointValuePair;
+import org.apache.commons.math3.optim.SimpleBounds;
+import org.apache.commons.math3.optim.linear.LinearObjectiveFunction;
 import org.apache.commons.math3.optim.linear.SimplexSolver;
+import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
+import org.ojalgo.access.Access1D;
 import org.ojalgo.optimisation.ExpressionsBasedModel;
 import org.ojalgo.optimisation.Optimisation;
+import org.ojalgo.optimisation.Variable;
 
 final class CommonsMathSimplexSolver implements Optimisation.Solver {
 
     public static final ExpressionsBasedModel.Integration<CommonsMathSimplexSolver> INTEGRATION = new ExpressionsBasedModel.Integration<CommonsMathSimplexSolver>() {
 
         public CommonsMathSimplexSolver build(final ExpressionsBasedModel model) {
+
+            Access1D<Double> linearFactors = model.objective().toFunction().getLinearFactors();
+
+            LinearObjectiveFunction obj = new LinearObjectiveFunction(linearFactors.toRawCopy1D(), 0.0);
+
+            Variable tmpVariable;
+            List<Variable> variables = model.getVariables();
+
+            double[] lowerBounds = new double[variables.size()];
+            double[] upperBounds = new double[variables.size()];
+            for (int v = 0; v < variables.size(); v++) {
+                tmpVariable = variables.get(v);
+                lowerBounds[v] = tmpVariable.getAdjustedLowerLimit();
+                upperBounds[v] = variables.get(v).getAdjustedUpperLimit();
+            }
+            SimpleBounds simpleBounds = new SimpleBounds(lowerBounds, upperBounds);
+
+            GoalType goalType = model.isMaximisation() ? GoalType.MAXIMIZE : GoalType.MINIMIZE;
+
+            OptimizationData[] data = new OptimizationData[] { goalType, obj, simpleBounds };
+
             // TODO Auto-generated method stub
             return null;
         }
 
         public boolean isCapable(final ExpressionsBasedModel model) {
-            // TODO Auto-generated method stub
-            return false;
+            return !model.isAnyVariableInteger() && !model.isAnyExpressionQuadratic();
         }
 
     };
 
-    private final SimplexSolver myDelegate = null;
+    private final OptimizationData[] myModelData;
+    private final Optimisation.Options myOptions;
 
-    public Result solve(final Result kickStarter) {
-        // TODO Auto-generated method stub
-        return null;
+    public Optimisation.Result solve(final Result kickStarter) {
+
+        InitialGuess guess = new InitialGuess(kickStarter.toRawCopy1D());
+
+        SimplexSolver solver = new SimplexSolver();
+
+        PointValuePair solutionAndValue = solver.optimize(myModelData);
+
+        Optimisation.State state = Optimisation.State.OPTIMAL;
+        double value = solutionAndValue.getValue();
+        Access1D<Double> solution = Access1D.wrap(solutionAndValue.getPoint());
+
+        Optimisation.Result result = new Optimisation.Result(state, value, solution);
+
+        return result;
+    }
+
+    CommonsMathSimplexSolver(OptimizationData[] modelData, Optimisation.Options options) {
+        super();
+        myModelData = modelData;
+        myOptions = options;
     }
 
 }
