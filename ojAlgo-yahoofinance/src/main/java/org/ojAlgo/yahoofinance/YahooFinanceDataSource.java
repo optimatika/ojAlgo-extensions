a779da9b50fb2d6494442218442fde103079e46e
apete <apete@optimatika.se>
2018-12-02 13:22:06 +0100

    yahoofinance

---
+++ ojAlgo-yahoofinance/src/main/java/org/ojAlgo/yahoofinance/YahooFinanceDataSource.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 1997-2018 Optimatika
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.ojAlgo.yahoofinance;
+
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.ojalgo.array.DenseArray;
+import org.ojalgo.finance.data.DatePrice;
+import org.ojalgo.finance.data.FinanceData;
+import org.ojalgo.finance.data.parser.YahooParser;
+import org.ojalgo.netio.BasicLogger;
+import org.ojalgo.series.BasicSeries;
+import org.ojalgo.series.CalendarDateSeries;
+import org.ojalgo.type.CalendarDate;
+import org.ojalgo.type.CalendarDateUnit;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+public final class YahooFinanceDataSource implements FinanceData {
+
+    private static final TemporalAdjuster FRIDAY = TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY);
+    private static final TemporalAdjuster LAST_DAY_OF_MONTH = TemporalAdjusters.lastDayOfMonth();
+
+    public static YahooFinanceDataSource of(String symbol, CalendarDateUnit resolution) {
+        return new YahooFinanceDataSource(symbol, resolution);
+    }
+
+    private final Stock myFetcher;
+    private final CalendarDateUnit myResolution;
+
+    YahooFinanceDataSource(String symbol, CalendarDateUnit resolution) {
+
+        super();
+
+        Stock fetcher = null;
+
+        try {
+            fetcher = YahooFinance.get(symbol);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        myFetcher = fetcher;
+        myResolution = resolution;
+    }
+
+    public List<DatePrice> getHistoricalPrices() {
+        try {
+            return myFetcher.getHistory().stream().map(hq -> {
+                YahooParser.Data dp = new YahooParser.Data(CalendarDate.toLocalDate(hq.getDate()));
+                dp.adjustedClose = hq.getAdjClose().doubleValue();
+                dp.close = hq.getClose().doubleValue();
+                dp.high = hq.getHigh().doubleValue();
+                dp.low = hq.getLow().doubleValue();
+                dp.open = hq.getOpen().doubleValue();
+                dp.volume = hq.getVolume().doubleValue();
+                return dp;
+            }).sorted().collect(Collectors.toList());
+        } catch (final Exception exception) {
+            exception.printStackTrace();
+            BasicLogger.error("Fetch problem for {}!", myFetcher.getClass().getSimpleName());
+            BasicLogger.error("Symbol & Resolution: {} & {}", myFetcher.getSymbol(), myResolution);
+            return Collections.emptyList();
+        }
+    }
+
+    public CalendarDateSeries<Double> getPriceSeries() {
+        return this.getPriceSeries(myResolution, LocalTime.NOON, ZoneOffset.UTC);
+    }
+
+    public CalendarDateSeries<Double> getPriceSeries(CalendarDateUnit resolution) {
+        return this.getPriceSeries(resolution, LocalTime.NOON, ZoneOffset.UTC);
+    }
+
+    public CalendarDateSeries<Double> getPriceSeries(CalendarDateUnit resolution, LocalTime time, ZoneId zoneId) {
+
+        CalendarDateSeries<Double> retVal = new CalendarDateSeries<>(resolution);
+
+        for (DatePrice datePrice : this.getHistoricalPrices()) {
+            retVal.put(CalendarDate.valueOf(datePrice.key.atTime(time).atZone(zoneId)), datePrice.getPrice());
+        }
+
+        return retVal;
+    }
+
+    public BasicSeries.NaturallySequenced<LocalDate, Double> getPriceSeries(DenseArray.Factory<Double> denseArrayFactory) {
+
+        BasicSeries.NaturallySequenced<LocalDate, Double> retVal = BasicSeries.LOCAL_DATE.build(denseArrayFactory);
+
+        LocalDate adjusted;
+        for (DatePrice datePrice : this.getHistoricalPrices()) {
+            switch (myResolution) {
+            case MONTH:
+                adjusted = (LocalDate) LAST_DAY_OF_MONTH.adjustInto(datePrice.key);
+                break;
+            case WEEK:
+                adjusted = (LocalDate) FRIDAY.adjustInto(datePrice.key);
+                break;
+            default:
+                adjusted = datePrice.key;
+                break;
+            }
+            retVal.put(adjusted, datePrice.getPrice());
+        }
+
+        return retVal;
+    }
+
+    public CalendarDateSeries<Double> getPriceSeries(LocalTime time, ZoneId zoneId) {
+        return this.getPriceSeries(myResolution, time, zoneId);
+    }
+
+    public String getSymbol() {
+        return myFetcher.getSymbol();
+    }
+
+}
